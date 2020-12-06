25e6d8e24deeff512f457f7046022d3c4fc4a965
Terence Lawson <terence.lawson@student.vinci.be>
2020-12-07 00:24:31 +0100

    New display locations list

--- src/components/DisplayQrCodes.vue
+++ src/components/DisplayQrCodes.vue
@@ -1,54 +1,42 @@
 <template>
   <div>
     <div class="row container-fluid mw-100">
-      <div class=" container col-4">
+      <div class=" pl-0 pr-0 container col-4">
         <div class="list-group vh-100">
-          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start ">
-            <div class="d-flex w-100 justify-content-between">
-              <h5 class="mb-1">List group item heading</h5>
-            </div>
-            <p class="mb-1 text-left">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus</p>
-          </a>
-          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start ">
-            <div class="d-flex w-100 justify-content-between">
-              <h5 class="mb-1">List group item heading</h5>
-            </div>
-            <p class="mb-1 text-left">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus</p>
-          </a>
-          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start ">
-            <div class="d-flex w-100 justify-content-between">
-              <h5 class="mb-1">List group item heading</h5>
-            </div>
-            <p class="mb-1 text-left">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus</p>
-          </a>
-          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start ">
-            <div class="d-flex w-100 justify-content-between">
-              <h5 class="mb-1">List group item heading</h5>
-            </div>
-            <p class="mb-1 text-left">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus</p>
-          </a>
-
+          <QrCodeListItem v-bind:locations="locations"/>
         </div>
       </div>
-      <div class=" card col-8 mh-100 min-vh-100">
-        <div class="mh-100">
-          <img src="../../src/assets/logo.png" class=" rounded mx-auto d-block" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-
-      </div>
+      <QrCodeDetailView/>
     </div>
   </div>
 </template>
 
 <script>
+import QrCodeListItem from "@/components/QrCodeListItem";
+import QrCodeDetailView from "@/components/QrCodeDetailView";
+import axios from "axios";
+
 export default {
-  name: "DisplayQrCodes"
+  name: "DisplayQrCodes",
+  components: {QrCodeDetailView, QrCodeListItem},
+  data() {
+    return {
+      locations: null,
+      errored: false
+    }
+
+  },
+  mounted() {
+    axios
+        .get('https://pfe-backend-dev.herokuapp.com/professionals/locations')
+        .then(response => (this.locations = response.data))
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false);
+  }
+
 }
 </script>
 
