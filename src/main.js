83ec7b42788abc0e883b24c07f7128f3a6ce12cf
Nicolas <nicolas.gerard@student.vinci.be>
2020-12-08 14:21:00 +0100

    added navigation guards

--- src/main.js
+++ src/main.js
@@ -3,16 +3,25 @@ import App from './App.vue'
 import router from './router'
 import Vuex from 'vuex'
 import Session from 'vue-session'
+import store from './components/blockCovidStore'
 
 
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
 Vue.use(Session)
+router.beforeEach((to, from, next) => {
+  if (to.name==='DisplayQrCodes' && !store.state.isConnected){
+    next({ name: 'Login' });
+  } else if(to.name==='Register' && store.state.isConnected) {
+    next({name: 'Home'})
+  }
+   else {
+    next();
+  }
+});
 
 new Vue({
   router,
   render: h => h(App),
-
-
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ const routes = [
   {
     path: '/displayQrCodes',
     name: 'DisplayQrCodes',
-   component: DisplayQrCodes
+    component: DisplayQrCodes
   }
 ]
 
